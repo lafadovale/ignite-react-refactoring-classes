c43f1bde1bc04c930c65e46117c9b37d60db2d2e
Lafaiete do Vale <lafa.vale@gmail.com>
2023-04-03 19:10:36 -0300

    Header and Input components refactored

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,29 +1,29 @@
-import { Component } from "react";
 import { FiPlusSquare } from "react-icons/fi";
 
 import { Container } from "./styles";
-import Logo from "../../assets/logo.svg";
+// import Logo from "../../assets/logo.svg"; # Dando erro
+const Logo = require("../../assets/logo.svg") as string; // Resolvido
 
-export class Header extends Component {
-  render() {
-    const { openModal } = this.props;
+interface HeaderProps {
+  openModal: () => void;
+}
 
-    return (
-      <Container>
-        <header>
-          <img src={Logo} alt="GoRestaurant" />
-          <nav>
-            <div>
-              <button type="button" onClick={openModal}>
-                <div className="text">Novo Prato</div>
-                <div className="icon">
-                  <FiPlusSquare size={24} />
-                </div>
-              </button>
-            </div>
-          </nav>
-        </header>
-      </Container>
-    );
-  }
+export function Header({ openModal }: HeaderProps) {
+  return (
+    <Container>
+      <header>
+        <img src={Logo} alt="GoRestaurant" />
+        <nav>
+          <div>
+            <button type="button" onClick={openModal}>
+              <div className="text">Novo Prato</div>
+              <div className="icon">
+                <FiPlusSquare size={24} />
+              </div>
+            </button>
+          </div>
+        </nav>
+      </header>
+    </Container>
+  );
 }
