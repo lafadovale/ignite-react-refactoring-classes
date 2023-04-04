3e970bcf7b8763b7a3cd12c1417ebdfc3fcaaed7
Lafaiete do Vale <lafa.vale@gmail.com>
2023-04-03 21:58:01 -0300

    Component ModalEditFood refactored

--- src/components/ModalAddFood/index.tsx
+++ src/components/ModalAddFood/index.tsx
@@ -1,9 +1,11 @@
 import { useRef } from "react";
+import { FormHandles } from "@unform/core";
+
+import { Modal } from "../Modal";
+import { Input } from "../Input";
 import { FiCheckSquare } from "react-icons/fi";
 
 import { Form } from "./styles";
-import { Modal } from "../Modal";
-import { Input } from "../Input";
 
 interface ModalAddFoodProps {
   isOpen: boolean;
@@ -23,7 +25,7 @@ export function ModalAddFood({
   setIsOpen,
   handleAddFood,
 }: ModalAddFoodProps) {
-  const formRef = useRef(null);
+  const formRef = useRef<FormHandles>(null);
 
   const handleSubmit = async (data: AddFood) => {
     handleAddFood(data);
